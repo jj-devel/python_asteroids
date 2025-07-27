f87e2eb2cde3374faef5987f0a77456696f728f5
jj-devel <jj.dev3l@gmail.com>
2025-07-27 14:26:13 -0400

    Created pygame clock object and set the fps to 60, while also logging the delta time in {dt}.

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    FPS = 60
     while True:
         for event  in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (00, 00, 00))
+        screen.fill((00, 00, 00))
         pygame.display.flip()
+        clock.tick(FPS)
+        dt = clock.get_time() / 1000 
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
