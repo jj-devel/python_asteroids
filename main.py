b14b985f2d704db6e5e1faef79eecda11771621e
jj-devel <jj.dev3l@gmail.com>
2025-07-27 15:16:28 -0400

    Added CircleShape parent class. Added Player sub-class of CircleShape. Added {PLAYER_WIDTH} and {PLAYER_HEIGHT} constants. Finally, added some comments to seperate parts of the {main()} function.

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+
+    # main() function calls
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     FPS = 60
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2),)
+
+    # Game loop
     while True:
+        # Call for game loop exit protocol
         for event  in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((00, 00, 00))
+
+        # Main section of game loop
+        player.draw(screen)
+
+        # End section of game loop
         pygame.display.flip()
         clock.tick(FPS)
         dt = clock.get_time() / 1000 
